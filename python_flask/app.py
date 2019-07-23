af0ade1b8c01f2adc471b41b988f79038c035b1e
Liang-Chun Tsai <lits@microsoft.com>
2019-07-23 14:16:24 -0700

    Add video register

--- python_flask/app.py
+++ python_flask/app.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from flask import request
+import os
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/video', methods=['POST'])
+def store_video():
+    video = request.files["video"]
+    video.save("static/test.mp4")
+    return 'Done'
+
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
